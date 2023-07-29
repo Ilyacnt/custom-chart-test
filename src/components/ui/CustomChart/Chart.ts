7b3f441a2dd192d94a18f46d7a7d364e1fea6b70
Ilyacnt <ilyagt2012@gmail.com>
2023-07-29 23:48:42 +0300

    Dragging feature

--- src/components/ui/CustomChart/Chart.ts
+++ src/components/ui/CustomChart/Chart.ts
@@ -4,21 +4,48 @@ class Chart {
     private readonly colors = {
         backgroundColor: '#171A1E',
         gridColor: '#384659',
+        curosorGridColor: '#AEB6B7',
         positiveColor: '#5DC887',
         negativeColor: '#E35561',
     }
-    private readonly scaleResolution: number = 2
+    private dataValues: DataPoint[] | null = null
+
+    private initialParentWidth: number = 0
+    private initialParentHeight: number = 0
+
     private canvas: HTMLCanvasElement
     private ctx: CanvasRenderingContext2D
-    private dataValues: DataPoint[] | null = null
+
+    private cursorX: number = 0
+    private cursorY: number = 0
+
+    private readonly scaleResolution: number = 2
+
     private zoom: number = 1
+    private readonly maxZoom = 3
+    private readonly minZoom = 0.5
+    private zoomHorizontal: number = 1
+    private zoomVertical: number = 1
+
+    private isDragging: boolean = false
+    private dragStartX: number = 0
+    private dragStartY: number = 0
+
+    private offsetX: number = 0
+    private offsetY: number = 0
+    private maxOffsetX: number = 0
+    private maxOffsetY: number = 0
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas as HTMLCanvasElement
         this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
         this.setBackgroundColor(this.colors.backgroundColor)
         this.resizeCanvas()
+        canvas.addEventListener('mousedown', this.handleMouseDown.bind(this))
+        canvas.addEventListener('mousemove', this.handleMouseMove.bind(this))
+        canvas.addEventListener('mouseup', this.handleMouseUp.bind(this))
         window.addEventListener('resize', this.resizeCanvas.bind(this))
+        canvas.addEventListener('wheel', this.handleMouseWheel.bind(this))
     }
 
     public render(data: DataPoint[], zoom: number) {
@@ -27,11 +54,66 @@ class Chart {
         this.draw()
     }
 
+    private setBackgroundColor(color: string) {
+        this.ctx.fillStyle = color
+        this.ctx.fillRect(0, 0, this.canvas!.width, this.canvas!.height)
+    }
+
+    private handleMouseDown(event: MouseEvent) {
+        this.isDragging = true
+        this.dragStartX = event.clientX
+        this.dragStartY = event.clientY
+    }
+
+    private handleMouseUp() {
+        this.isDragging = false
+    }
+
+    private handleMouseMove(event: MouseEvent) {
+        const rect = this.canvas.getBoundingClientRect()
+        this.cursorX = (event.clientX - rect.left) * this.scaleResolution
+        this.cursorY = (event.clientY - rect.top) * this.scaleResolution
+
+        if (this.isDragging) {
+            const deltaX = event.clientX - this.dragStartX
+            const deltaY = event.clientY - this.dragStartY
+            this.dragStartX = event.clientX
+            this.dragStartY = event.clientY
+
+            this.offsetX += deltaX
+            this.offsetY += deltaY
+
+            this.draw()
+        } else {
+            this.draw()
+        }
+    }
+
+    private handleMouseWheel(event: WheelEvent) {
+        // if (!event.ctrlKey) return
+        const zoomDelta = event.deltaY > 0 ? 1.1 : 0.9
+        this.zoomHorizontalHandler(this.zoomHorizontal * zoomDelta)
+        this.zoomVerticalHandler(this.zoomVertical * zoomDelta)
+
+        this.dataValues && this.render(this.dataValues, this.zoomHorizontal)
+        event.preventDefault()
+    }
+
+    public zoomHorizontalHandler(zoomLevel: number) {
+        this.zoomHorizontal = Math.max(0.1, Math.min(10, zoomLevel))
+        this.draw()
+    }
+
+    public zoomVerticalHandler(zoomLevel: number) {
+        this.zoomVertical = Math.max(0.1, Math.min(10, zoomLevel))
+        this.draw()
+    }
+
     private draw() {
         if (!this.dataValues) return
-
         this.clearCanvas()
         this.drawGrid()
+        this.drawCursorLines()
         this.ctx.strokeStyle = this.colors.negativeColor
         this.ctx.lineWidth = 5
         this.ctx.beginPath()
@@ -42,39 +124,101 @@ class Chart {
         this.ctx.stroke()
     }
 
-    public setCanvasResolution(width: number, height: number) {
-        this.canvas.width = width
-        this.canvas.height = height
-    }
-
     private drawGrid() {
-        const gridLinesCount = 11
-        const horizontalSpacing = this.canvas!.height / (gridLinesCount - 1)
-
-        const verticalSpacing = this.canvas!.width / 9
+        const { horizontalSpacing, verticalSpacing } = this.calculateGridSpacing()
 
         this.ctx.beginPath()
-        for (let i = 0; i < gridLinesCount; i++) {
-            const y = horizontalSpacing * i
-            this.ctx.moveTo(0, y)
-            this.ctx.lineTo(this.canvas!.width, y)
+        for (let y = 0; y < this.canvas!.height; y += horizontalSpacing) {
+            this.ctx.moveTo(0, y + this.offsetY)
+            this.ctx.lineTo(this.canvas!.width, y + this.offsetY)
         }
         this.ctx.strokeStyle = this.colors.gridColor
+        this.ctx.lineWidth = 1
         this.ctx.stroke()
 
         this.ctx.beginPath()
-        for (let i = 1; i < 9; i++) {
-            const x = verticalSpacing * i
-            this.ctx.moveTo(x, 0)
-            this.ctx.lineTo(x, this.canvas!.height)
+        for (let x = 0; x < this.canvas!.width; x += verticalSpacing) {
+            this.ctx.moveTo(x + this.offsetX, 0)
+            this.ctx.lineTo(x + this.offsetX, this.canvas!.height)
         }
         this.ctx.strokeStyle = this.colors.gridColor
         this.ctx.stroke()
     }
 
-    public setBackgroundColor(color: string) {
-        this.ctx.fillStyle = color
-        this.ctx.fillRect(0, 0, this.canvas!.width, this.canvas!.height)
+    private drawCursorLines() {
+        this.ctx.setLineDash([10, 15])
+        this.ctx.strokeStyle = this.colors.curosorGridColor
+        this.ctx.lineWidth = 1
+
+        this.ctx.beginPath()
+        this.ctx.moveTo(0, this.cursorY)
+        this.ctx.lineTo(this.canvas.width, this.cursorY)
+        this.ctx.stroke()
+
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.cursorX, 0)
+        this.ctx.lineTo(this.cursorX, this.canvas.height)
+        this.ctx.stroke()
+
+        this.ctx.setLineDash([])
+        this.drawCursorCoordinates()
+    }
+
+    private drawCursorCoordinates() {
+        this.ctx.font = '20px "Rubik", sans-serif'
+
+        const cursorTextX = `X: ${this.cursorX.toFixed(2)}`
+        const cursorTextY = `Y: ${this.cursorY.toFixed(2)}`
+        const textOffsetX = 20
+        const textOffsetY = -45
+
+        this.ctx.fillStyle = this.colors.curosorGridColor
+        this.ctx.fillText(cursorTextX, this.cursorX + textOffsetX, this.cursorY + textOffsetY)
+        this.ctx.fillText(cursorTextY, this.cursorX + textOffsetX, this.cursorY + textOffsetY + 25)
+    }
+
+    private calculateGridSpacing() {
+        const maxHorizontalLines = 11
+        const maxVerticalLines = 8
+        const horizontalLines = Math.ceil(maxHorizontalLines / this.zoomVertical)
+        const verticalLines = Math.ceil(maxVerticalLines / this.zoomHorizontal)
+
+        let horizontalSpacing = this.canvas!.height / (horizontalLines - 1)
+        let verticalSpacing = this.canvas!.width / (verticalLines - 1)
+
+        if (this.zoomHorizontal > this.maxZoom) {
+            this.zoomHorizontal = this.maxZoom
+        }
+        if (this.zoomVertical > this.maxZoom) {
+            this.zoomVertical = this.maxZoom
+        }
+        if (this.zoomHorizontal < this.minZoom) {
+            this.zoomHorizontal = this.minZoom
+        }
+        if (this.zoomVertical < this.minZoom) {
+            this.zoomVertical = this.minZoom
+        }
+
+        const smoothnessFactor = 0.1
+        horizontalSpacing = this.lerp(
+            horizontalSpacing,
+            this.canvas!.height / (maxHorizontalLines - 1),
+            smoothnessFactor
+        )
+        verticalSpacing = this.lerp(
+            verticalSpacing,
+            this.canvas!.width / (maxVerticalLines - 1),
+            smoothnessFactor
+        )
+
+        return {
+            horizontalSpacing,
+            verticalSpacing,
+        }
+    }
+
+    private lerp(start: number, end: number, amount: number) {
+        return (1 - amount) * start + amount * end
     }
 
     private clearCanvas(): void {
@@ -83,8 +227,16 @@ class Chart {
 
     private resizeCanvas() {
         if (!this.canvas || !this.canvas.parentElement) return
-        this.canvas.width = this.canvas.parentElement.clientWidth * this.scaleResolution
-        this.canvas.height = this.canvas.parentElement.clientHeight * this.scaleResolution
+
+        const parentWidth = this.canvas.parentElement.clientWidth
+        const parentHeight = this.canvas.parentElement.clientHeight
+
+        this.canvas.width = parentWidth * this.scaleResolution
+        this.canvas.height = parentHeight * this.scaleResolution
+
+        this.zoomHorizontal = this.canvas.width / parentWidth
+        this.zoomVertical = this.canvas.height / parentHeight
+
         this.draw()
     }
 }
